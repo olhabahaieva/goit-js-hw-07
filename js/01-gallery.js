c28e3d42e22ca85a743d75e4ee973ac02fcf8602
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-02 18:38:59 +0300

    modal window works

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -19,10 +19,7 @@ gallery.addEventListener("click", onClick);
 function onClick(evt){
     const instance = basicLightbox.create(`
     <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
+    <img src="assets/images/image.png" width="800" height="600">
     </div>
 `)
 
