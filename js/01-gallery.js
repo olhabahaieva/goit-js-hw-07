b815989cec9c2801bef5ba616bbe0e774bd525d1
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-02 17:56:30 +0300

    created html markup with JS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Initial html element to add new pictures
+const gallery = document.querySelector("ul.gallery");
+
+// New structure to add to the initial html document
+const markup = galleryItems.map(({preview, original, description}) =>
+ `<li>
+    <img src="${preview}"
+    alt="${description}"/>
+ </li>`
+)
+gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
