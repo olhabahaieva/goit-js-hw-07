7d8c1db209b90f34a8ea0782c9497cfca30bba0f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-02 20:15:49 +0300

    added adjusted JS file from 1 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+// Initial html element to add new pictures
+const gallery = document.querySelector("ul.gallery");
+
+// New structure to add to the initial html document
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+ `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+console.log(galleryItems);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const image = evt.target;
+  const galleryItem = galleryItems.find(
+    (item) => item.preview === image.src
+  );
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${galleryItem.original}" class="gallery__item">
+    </div>
+`);
+
+  instance.show();
+}
+
 console.log(galleryItems);
