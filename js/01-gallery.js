097f233a08ed4b9305a57818324fcb7546dfc7f4
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-02 18:25:00 +0300

    basicLightbox works onClick

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,3 +13,18 @@ const markup = galleryItems.map(({preview, original, description}) =>
 ).join('');
 gallery.insertAdjacentHTML("beforeend", markup);
 console.log(galleryItems);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt){
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
+}
